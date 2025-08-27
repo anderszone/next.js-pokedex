3563969d7b6e61edd65ffa2eb8bae68ba6ea0334
Anders Johansson <inboxanders@gmail.com>
2025-08-27 22:18:02 +0200

    Started on CSS for the featured section

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -2,9 +2,9 @@ import PokemonCard from "@/components/PokemonCard";
 
 export default function Featured() {
   return (
-    <section>
-        <h2>Featured Pokémons</h2>
-        <ul>
+    <section className="content-grid bg-gradient padding-block-large">
+        <h2 className="text-center">Featured Pokémons</h2>
+        <ul className="flex space-between">
             <PokemonCard />
             <PokemonCard />
             <PokemonCard />

fcb5e3bdb612fb5c97e7ed05b9ebe2e31bc21aaa
Anders Johansson <inboxanders@gmail.com>
2025-09-04 17:54:55 +0200

    Improved styling of pokemon cards and changed the favicon

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -21,15 +21,19 @@ export default function PokemonCard({ pokemon }: PokemonCardProps) {
   // Rendera kortet med Pokémon-information
   return (
     <>
-        <Image src={pokemon.sprite} alt={pokemon.name} width={60} height={60} />
-        <div>#{pokemon.id}</div>
-        <h3>{pokemon.name}</h3>
-        <div>{pokemon.types.join(", ")}</div>
-        <ul>
+        <Image className="pokemon-image margin-bottom-small" src={pokemon.sprite} alt={pokemon.name} width={100} height={100} />
+        <div className="pokemon-id margin-bottom-small">#{pokemon.id}</div>
+        <h3 className="margin-bottom-small capitalize">{pokemon.name}</h3>
+        <ul className="flex gap-tiny margin-bottom-medium">
+          {pokemon.types.map(type => (
+            <li key={type} className="pokemon-tag">{type}</li>
+          ))}
+        </ul>
+        <ul className="stretch">
           {/* Loopar igenom stats för Pokemon (HP, Attack, Defense) och visar dessa */}
           {filteredStats.map(stat => (
-            <li key={stat.name}>
-              <span>{stat.name}</span>: <span>{stat.value}</span>
+            <li key={stat.name} className="flex space-between bold">
+              <span className={stat.name === "hp" ? "uppercase" : "capitalize"}>{stat.name}</span>&nbsp;<span>{stat.value}</span>
             </li>
           ))}
         </ul>
